95f2bdb2dafd81dbd7fbfd1c9f9d4ea53b8b6f9b
khan Tanveer <kt224945@gmail.com>
2021-03-13 17:32:32 +0530

    commit

--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   List,
   ListItem,
@@ -6,29 +6,56 @@ import {
   Avatar,
   ImageIcon,
   ListItemText,
-  Button,
+  Modal,
 } from "@material-ui/core";
 import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import { makeStyles } from "@material-ui/core/styles";
 import "./Todo.css";
 import db from "./firebase";
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    background: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 const Todo = (props) => {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
   return (
-    <List className="todo__list">
-      <ListItem>
-        <ListItemAvatar>
-          {/* <Avatar>
+    <>
+      <Modal open={open} onClose={(e) => setOpen(false)}>
+        <div>
+          <h1>I am a model</h1>
+          <button onClick={(e) => setOpen(false)}></button>
+        </div>
+      </Modal>
+      <List className="todo__list">
+        <ListItem>
+          <ListItemAvatar>
+            {/* <Avatar>
             <ImageIcon />
           </Avatar> */}
-        </ListItemAvatar>
-        <ListItemText primary={props.todo.todo} secondary="Dummy deadline..." />
-      </ListItem>
-      <DeleteForeverIcon
-        onClick={(e) => db.collection("todos").doc(props.todo.id).delete()}
-      >
-        Delete Me
-      </DeleteForeverIcon>
-    </List>
+          </ListItemAvatar>
+          <ListItemText
+            primary={props.todo.todo}
+            secondary="Dummy deadline..."
+          />
+        </ListItem>
+        <button onClick={(e) => setOpen(true)}>Edit me </button>
+        <DeleteForeverIcon
+          onClick={(e) => db.collection("todos").doc(props.todo.id).delete()}
+        >
+          Delete Me
+        </DeleteForeverIcon>
+      </List>
+    </>
     // <div>
     //   <li>{props.text}</li>
     // </div>
